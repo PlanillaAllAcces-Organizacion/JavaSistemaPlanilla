8f59314dd366db0ecf340a492ccba6c7f3956e26
Juan_Vasquez <vl25001@esfe.agape.edu.sv>
2025-06-06 15:38:34 -0600

    Se creo el archivo para conectar con la DB.

--- src/main/java/Grupo05/Persistencia/ConnectionManager.java
+++ src/main/java/Grupo05/Persistencia/ConnectionManager.java
@@ -1,4 +1,68 @@
 package Grupo05.Persistencia;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionManager {
+
+    private static final String  STR_CONNECTION = "jdbc:sqlserver://JC39012\\SQLEXPRESS; "+
+            "encrypt=true; " +
+            "database=Paysheet1; " +
+            "trustServerCertificate=true;" +
+            "user=java2025;" +
+            "password=12345";
+
+
+    private Connection connection;
+
+    private static ConnectionManager instance;
+
+
+    private ConnectionManager() {
+        this.connection = null;
+        try {
+
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+            // Si el driver no se encuentra, se lanza una excepción indicando el error.
+            throw new RuntimeException("Error al cargar el driver JDBC de SQL Server", e);
+        }
+    }
+
+
+    public synchronized Connection connect() throws SQLException {
+        if (this.connection == null || this.connection.isClosed()) {
+            try {
+                this.connection = DriverManager.getConnection(STR_CONNECTION);
+            } catch (SQLException exception) {
+
+                throw new SQLException("Error al conectar a la base de datos: " + exception.getMessage(), exception);
+            }
+        }
+        // Retorna la conexión (ya sea la existente o la recién creada).
+        return this.connection;
+    }
+
+
+    public void disconnect() throws SQLException {
+        if (this.connection != null) {
+            try {
+                this.connection.close();
+            } catch (SQLException exception) {
+
+                throw new SQLException("Error al cerrar la conexión: " + exception.getMessage(), exception);
+            } finally {
+
+                this.connection = null;
+            }
+        }
+    }
+
+    public static synchronized ConnectionManager getInstance() {
+        if (instance == null) {
+            instance = new ConnectionManager();
+        }
+        return instance;
+    }
 }
